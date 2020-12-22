62f39255b2f0bfaf0bb33e1d8c475d955b12b961
Shane Neubauer <shanen@gmail.com>
2020-12-22 16:35:25 +0100

    Supports different rooms and some aesthetic tweaks

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-// const SIGNALING_SERVER_URL = 'https://signal-7lz3zezpea-ew.a.run.app/';
-const SIGNALING_SERVER_URL = 'http://localhost:8090';
+const SIGNALING_SERVER_URL = 'https://signal-7lz3zezpea-ew.a.run.app/';
+// const SIGNALING_SERVER_URL = 'http://localhost:8090';
 const PC_CONFIG = {};
 
 const socket = io(SIGNALING_SERVER_URL, {
@@ -8,6 +8,16 @@ const socket = io(SIGNALING_SERVER_URL, {
 
 socket.on('data', (data) => {
   handleSignalingData(data);
+});
+
+socket.on('connect', () => {
+  console.log('Connected to signal server')
+});
+
+socket.on('hello', sid => {
+  console.log('Session started with ID', sid);
+  socket.emit('meetingid', sessionMeetingId);
+  showHappyMessage('Connected');
 });
 
 socket.on('ready', (data) => {
@@ -22,9 +32,17 @@ let sendData = (data) => {
 
 let pc;
 let localStream;
+let sessionMeetingId;
 
+const btnStartNewMeeting = document.getElementById('btn-start-meeting');
+const btnJoinMeeting = document.getElementById('btn-join-meeting');
+const inputDisplayMeetingId = document.getElementById('display-meeting-id');
+const inputEnterMeetingId = document.getElementById('input-meeting-id');
 const localStreamElement = document.getElementById('local-stream');
 const remoteStreamElement = document.getElementById('remote-stream');
+const happyMessageBox = document.getElementById('happy-message');
+const indifferentMessageBox = document.getElementById('indifferent-message');
+const sadMessageBox = document.getElementById('sad-message');
 
 async function getLocalStream() {
   console.log('Starting media');
@@ -36,8 +54,14 @@ async function getLocalStream() {
   localStream = stream;
 }
 
-function joinMeeting() {
-  socket.connect()
+function joinMeeting(meetingId) {
+  if (! meetingId) {
+    showSadMessage('Please enter a meeting ID')
+    return;
+  }
+
+  showHappyMessage('Connecting');
+  socket.connect();
 }
 
 function createPeerConnection() {
@@ -47,8 +71,10 @@ function createPeerConnection() {
     pc.onaddstream = onAddStream;
     pc.addStream(localStream);
     console.log('PeerConnection created');
+    showHappyMessage('Connected');
   } catch (error) {
     console.error('PeerConnection failed: ', error);
+    showSadMessage('Connection failed. ¯\\_(ツ)_/¯');
   }
 }
 
@@ -102,11 +128,65 @@ function handleSignalingData(data) {
   }
 }
 
+function generateMeetingId() {
+  const chars = 'abcdefghijklmnopqrstuvwxyz';
+  const length = 9;
+  const result = []
+
+  for (let i = 0; i < length; i++) {
+    if (i > 0 && i%3 == 0) {
+      result.push('-')
+    }
+    result.push(chars.charAt(Math.floor(Math.random() * chars.length)))
+  }
+
+  return result.join('');
+}
+
+function showHappyMessage(message) {
+  happyMessageBox.style.display = 'block';
+  indifferentMessageBox.style.display = 'none';
+  sadMessageBox.style.display = 'none';
+  happyMessageBox.innerText = message;
+  setTimeout(hideAllMessages, 3000);
+}
+
+function showIndifferentMessage(message) {
+  happyMessageBox.style.display = 'none';
+  indifferentMessageBox.style.display = 'block';
+  sadMessageBox.style.display = 'none';
+  indifferentMessageBox.style.innerText = message;
+  setTimeout(hideAllMessages, 3000);
+}
+
+function showSadMessage(message) {
+  happyMessageBox.style.display = 'none';
+  indifferentMessageBox.style.display = 'none';
+  sadMessageBox.style.display = 'block';
+  sadMessageBox.innerText = message;
+  setTimeout(hideAllMessages, 3000);
+}
+
+function hideAllMessages() {
+  happyMessageBox.style.display = 'none';
+  indifferentMessageBox.style.display = 'none';
+  sadMessageBox.style.display = 'none';
+}
 
 document.addEventListener('DOMContentLoaded', function() {
   getLocalStream();
 
-  document.getElementById('btn-join-meeting').addEventListener('click', function() {
-    joinMeeting();
-  })
+  btnStartNewMeeting.addEventListener('click', function() {
+    const meetingId = generateMeetingId();
+    inputDisplayMeetingId.value = meetingId;
+    joinMeeting(meetingId);
+    sessionMeetingId = meetingId;
+  });
+
+  btnJoinMeeting.addEventListener('click', function() {
+    const meetingId = inputEnterMeetingId.value;
+    console.log('meetingId', meetingId);
+    joinMeeting(meetingId);
+    sessionMeetingId = meetingId;
+  });
 })
--- signal/index.js
+++ signal/index.js
@@ -1,27 +1,40 @@
-const httpServer = require('http').createServer();
+const httpServer = require('http').createServer()
 const io = require('socket.io')(httpServer, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
   },
-});
+})
 
-const PORT = process.env.PORT || 8090;
+const PORT = process.env.PORT || 8090
+
+const sessionRoomMap = {}
 
 io.on('connection', socket => {
   console.log(`[${socket.id}] New session`)
-});
-
-io.on('connect', socket => {
-  socket.join('room')
-
-  socket.emit('ready', {sid: socket.id})
-
-  socket.on('data', data => {
-    const bytes = JSON.stringify(data).length
-    console.log(`[${socket.id}] Message: ${data.type} (${bytes} bytes)`)
-    socket.to('room').emit('data', data)
-  })
 })
 
-httpServer.listen(PORT);
+io.on('connect', socket => {
+  socket.emit('hello', socket.id)
+
+  socket.on('meetingid', meetingId => {
+    console.log(`[${socket.id}] Join meeting room: ${meetingId}`)
+    socket.join(meetingId)
+    sessionRoomMap[socket.id] = meetingId
+    socket.emit('ready', meetingId)
+  })
+
+  socket.on('data', data => {
+    if (! socket.id in sessionRoomMap) {
+      throw Error('Bad handshake. Must join meeting room before sending data.')
+    }
+
+    const bytes = JSON.stringify(data).length
+    console.log(`[${socket.id}] Message: ${data.type} (${bytes} bytes)`)
+    socket.to(sessionRoomMap[socket.id]).emit('data', data)
+  })
+
+  // TODO: clean up sessionRoomMap on disconnect (otherwise memory leak)
+})
+
+httpServer.listen(PORT)
